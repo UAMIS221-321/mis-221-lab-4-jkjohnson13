bedbd73793531c79f30d2e2efaecbcf1fa2190d8
Jaden Johnson <jadenjohnson@Jadens-Air.attlocal.net>
2023-02-09 23:31:47 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿//*********START MAIN*********
+DisplayMenu();
+
+//*********END MAIN*********
+
+static void DisplayMenu()
+{
+    System.Console.WriteLine("Press enter to continue.");
+    string choice = Console.ReadLine();
+
+    while(choice != "3")
+    {
+        System.Console.WriteLine("\n1. Full Triangle\n2. Partial Triangle\n3. Exit");
+        choice = Console.ReadLine(); //update read
+
+        if(choice == "1")
+        {
+            GetFull();
+        }
+        else if(choice == "2")
+        {
+            GetPartial();
+        }
+        else if(choice == "3")
+        {
+
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid Input.");
+        }
+    }
+}
+
+static void GetFull()
+{
+    System.Console.WriteLine("Full Triangle");
+}
+
+static void GetPartial()
+{
+    System.Console.WriteLine("Partial Triangle");
+}
